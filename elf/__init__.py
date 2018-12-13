2d167ac68e83ac6897619afcc7f6d02c95415fb1
Mdjugev <mdjugev@gmail.com>
2018-12-13 11:41:58 +0300

    убрал лишние модули

--- elf/__init__.py
+++
@@ -1,7 +0,0 @@
-from elf.http_message import HTTPMessage
-from elf.http_stub import HTTPStub
-from elf.ssl import SSL
-from elf.template_xml import TemplateXML
-from elf.template_json import TemplateJSON
-from elf.debug import Debug
-from time import sleep
--- elf/ssl.py
+++
@@ -1,6 +0,0 @@
-class SSL:
-
-    def __init__(self, key, cert, cacert=None):
-        self.key = key
-        self.cert = cert
-        self.cacert = cacert
